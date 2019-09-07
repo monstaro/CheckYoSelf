195ac9b863001da1e708b5e0a1264d841803f427
Veronica Andrade <veronica726@gmail.com>
2019-09-07 16:45:48 -0600

    css changes that were corrected

--- task.js
+++ task.js
@@ -2,12 +2,9 @@ class Task {
   constructor(taskDescription, id){
     this.taskDescription = taskDescription;
     this.id = id || Date.now();
+    this.checkedOff = false;
   }
-  // addArrayToCard(listLocation){
-  //   listLocation.insertAdjacentHTML('beforeend', `<li class="main-task-items">
-  //     <img src="./images/checkbox-active.svg" class="main-task-icons">
-  //     <p class="main-task-text">${this.taskDescription}</p>
-  //   </li>`)
-  //
-  // }
+  updateTask(){
+    
+  }
 }
