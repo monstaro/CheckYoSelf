eb341630d493ef7bbca942c6662919c343e4d9fd
Cody Smith <codysmith2814@gmail.com>
2019-09-10 14:20:28 -0600

    Refactor, save arrays to local storage

--- todo-list.js
+++ todo-list.js
@@ -1,31 +1,15 @@
 class TodoList {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  constructor(title, id, array) {
-=======
-  constructor(id, title, array) {
-<<<<<<< HEAD
-    this.id = id || Date.now();
->>>>>>> 27a1f05bbcae632b51687d859aa0f1d1b26f6711
-=======
-=======
   constructor(id, title, array, buttonID) {
->>>>>>> 78bd67366bd9af713622f241165ae69f5218136f
     this.id = id || (Date.now() + Math.random());
->>>>>>> bf9728f45c35b6beaea7193ed7aaad39872b8c2a
     this.title = title;
-    this.id = id || Date.now();
-    this.tasks = array;
-<<<<<<< HEAD
     this.urgent = false;
-=======
+    this.tasks = array;
     this.buttonID = buttonID || (Date.now() + Math.random());
->>>>>>> 78bd67366bd9af713622f241165ae69f5218136f
   }
-  saveToStorage() {
-    // localStorage.setItem("name", name)
-    //call on card object
+  saveToStorage(bigArray) {
+    localStorage.setItem("bigArray", JSON.stringify(bigArray));
   }
+
   deleteFromStorage() {
 
   }
